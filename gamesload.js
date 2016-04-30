b7da70247b074ffb81def94ae702b9506c5b3a2f
Xtraloudninja <varun.ramani@gmail.com>
2016-04-29 22:20:57 -0700

    Completed migrate to embed

--- gamesload.js
+++ gamesload.js
@@ -2,50 +2,34 @@ function loadgame(game) {
     var viewer = document.getElementById('viewerholder');
     switch (game) {
         case 'ff2': 
-            viewerholder.innerHTML='<embed style="position: absolute; left: 20%; top: 5%" src="ff2.swf" quality="high" width="90%" height="90%" scale="exactfit" />'
+            viewerholder.innerHTML='<embed style="position: absolute; left: 35%; top: 5%" src="ff2.swf" quality="high" width="50%" height="90%" scale="exactfit" />'
             break;
         case 'slitherio':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 5%; top: 5%" src="http://slither.io" quality="high" width="90%" height="80%" scale="exactfit" />'
+            viewerholder.innerHTML='<embed style="position: absolute; left: 5%; top: 5%" src="http://slither.io" quality="high" width="90%" height="100%" scale="exactfit" />'
             break;
         case 'agario':
-            viewer.src = "http://agar.io";
-            viewer.height = "750px";
-            viewer.width = "1000px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 5%; top: 5%" src="http://agar.io" quality="high" width="90%" height="80%" scale="exactfit" />'
             break;
         case 'bloxorz':
-            viewer.src = "bloxorz.swf";
-            viewer.height = "350px";
-            viewer.width = "650px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 25%; top: 25%" src="bloxorz.swf" quality="high" width="50%" height="47%" scale="exactfit" />'
             break;
         case 'chibi-knight':
-            viewer.src = "chibi-knight.swf";
-            viewer.height = "530px";
-            viewer.width = "700px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 23%; top: 25%" src="chibi-knight.swf" quality="high" width="60%" height="70%" scale="exactfit" />'            
             break;
         case 'pacman':
-            viewer.src = "pacman.swf";
-            viewer.height = "700px";
-            viewer.width = "700px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 26%; top: 20%" src="pacman.swf" quality="high" width="50%" height="80%" scale="exactfit" />'
             break;
         case 'playgo':
-            viewer.src = "playgo.swf";
-            viewer.height = "500px";
-            viewer.width = "650px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 26%; top: 20%" src="playgo.swf" quality="high" width="50%" height="60%" scale="exactfit" />'
             break;
         case 'run2':
-            viewer.src = "run2.swf";
-            viewer.height = "510px";
-            viewer.width = "700px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 22%; top: 20%" src="run2.swf" quality="high" width="60%" height="70%" scale="exactfit" />'
             break;
         case 'stickwar':
-            viewer.src = "stickwar.swf";
-            viewer.height = "510px";
-            viewer.width = "700px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 16%; top: 20%" src="stickwar.swf" quality="high" width="70%" height="80%" scale="exactfit" />'
             break;
         case 'tombrunner':
-            viewer.src = "http://files.cdn.spilcloud.com/webgl/TR-13-01/index.html?gp=1&countryCode=US&siteid=500&channelid=100&siteLocale=en-US&spilStorageId=9829247031";
-            viewer.height = "750px";
-            viewer.width = "1000px";
+            viewerholder.innerHTML='<embed style="position: absolute; left: 16%; top: 20%" src="http://files.cdn.spilcloud.com/webgl/TR-13-01/index.html?gp=1&countryCode=US&siteid=500&channelid=100&siteLocale=en-US&spilStorageId=9829247031" quality="high" width="70%" height="80%" scale="exactfit" />'
             break;
         
     }
