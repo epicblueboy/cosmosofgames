707dbeea82954dc1d22409c116b875095173c830
Xtraloudninja <varun.ramani@gmail.com>
2016-04-30 11:55:58 -0700

    Completed all the games, and also added a lot of features

--- otherjs.js
+++ otherjs.js
@@ -1,10 +1,16 @@
-/*window.onkeydown = function (e) {
-    if (e.keyCode === 32 && e.target === document.body) {
-        e.preventDefault();
-        return false;
+
+var viewerholder = document.getElementById('viewerholder');
+/*if(window.chrome === undefined) {
+    viewerholder.innerHTML += '<br><div style="border: 5px solid blue; border-radius: 1vw; width: 200px; text-align: center;"><a href="https://www.google.com/chrome/browser/desktop/"><img src="http://img.talkandroid.com/uploads/2015/11/Chrome-Logo.png" style="height: 10vw; "></a><br><span style="color: white; font-size: 1.2em;">Cosmos of Games is designed for Google Chrome, the fast, simple, and secure web browser built for the modern web. If you do not have Chrome, click the Chrome icon to go download it.</span></div>';
+}*/
+if (window.chrome === undefined) {
+    if (confirm('Cosmos of Games works best on the Google Chrome browser. You do not seem to be running Google Chrome. Do you want to go download it now?')) {
+        location = 'https://www.google.com/chrome/browser/desktop/';
     }
-};*/
-var isChrome = !!window.chrome && !!window.chrome.webstore;
-if (isChrome === true) {
-    document.getElementById('viewerholder').innerHTML = ''
+    else {
+        alert('Since you are not running Google Chrome, some features in Cosmos of Games may or may not work.');
+    }
+}
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    alert('Cosmos of Games requires a mouse and keyboard. Please use your desktop computer or laptop.');
 }
