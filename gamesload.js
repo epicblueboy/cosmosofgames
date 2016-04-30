e7efe97ae3bbb16380cbfb36f0082f85fa1816ae
Xtraloudninja <varun.ramani@gmail.com>
2016-04-30 13:09:14 -0700

    Added more features

--- gamesload.js
+++ gamesload.js
@@ -2,34 +2,34 @@ function loadgame(game) {
     var viewer = document.getElementById('viewerholder');
     switch (game) {
         case 'ff2': 
-            viewerholder.innerHTML='<embed style="position: absolute; left: 35%; top: 5%" src="ff2.swf" quality="high" width="50%" height="90%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 35%; top: 5%" src="ff2.swf" quality="high" width="50%" height="90%" scale="exactfit" />';
             break;
         case 'slitherio':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 5%; top: 5%" src="http://slither.io" quality="high" width="90%" height="100%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 5%; top: 5%" src="http://slither.io" quality="high" width="90%" height="100%" scale="exactfit" />';
             break;
         case 'agario':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 5%; top: 5%" src="http://agar.io" quality="high" width="90%" height="80%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 5%; top: 5%" src="http://agar.io" quality="high" width="90%" height="80%" scale="exactfit" />';
             break;
         case 'bloxorz':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 25%; top: 25%" src="bloxorz.swf" quality="high" width="50%" height="47%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 25%; top: 25%" src="bloxorz.swf" quality="high" width="50%" height="47%" scale="exactfit" />';
             break;
         case 'chibi-knight':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 23%; top: 25%" src="chibi-knight.swf" quality="high" width="60%" height="70%" scale="exactfit" />'            
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 21%; top: 20%" src="chibi-knight.swf" quality="high" width="60%" height="75%" scale="exactfit" />';            
             break;
         case 'pacman':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 26%; top: 20%" src="pacman.swf" quality="high" width="50%" height="80%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 26%; top: 20%" src="pacman.swf" quality="high" width="50%" height="80%" scale="exactfit" />';
             break;
         case 'playgo':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 26%; top: 20%" src="playgo.swf" quality="high" width="50%" height="60%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 26%; top: 20%" src="playgo.swf" quality="high" width="50%" height="60%" scale="exactfit" />';
             break;
         case 'run2':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 22%; top: 20%" src="run2.swf" quality="high" width="60%" height="70%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 22%; top: 20%" src="run2.swf" quality="high" width="60%" height="70%" scale="exactfit" />';
             break;
         case 'stickwar':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 16%; top: 20%" src="stickwar.swf" quality="high" width="70%" height="80%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 16%; top: 20%" src="stickwar.swf" quality="high" width="70%" height="80%" scale="exactfit" />';
             break;
         case 'tombrunner':
-            viewerholder.innerHTML='<embed style="position: absolute; left: 16%; top: 20%" src="http://files.cdn.spilcloud.com/webgl/TR-13-01/index.html?gp=1&countryCode=US&siteid=500&channelid=100&siteLocale=en-US&spilStorageId=9829247031" quality="high" width="70%" height="80%" scale="exactfit" />'
+            viewerholder.innerHTML = '<embed style="position: absolute; left: 16%; top: 20%" src="http://files.cdn.spilcloud.com/webgl/TR-13-01/index.html?gp=1&countryCode=US&siteid=500&channelid=100&siteLocale=en-US&spilStorageId=9829247031" quality="high" width="70%" height="80%" scale="exactfit" />';
             break;
         
     }
--- otherjs.js
+++ otherjs.js
@@ -1,16 +1,32 @@
 
 var viewerholder = document.getElementById('viewerholder');
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    alert('Cosmos of Games works best with a mouse and keyboard. Please use your desktop computer or laptop.');
+}
+else {
+    if (window.chrome === undefined) {
+        if (confirm('Cosmos of Games works best on the Google Chrome browser. You do not seem to be running Google Chrome. Do you want to go download it now?')) {
+            location = 'https://www.google.com/chrome/browser/desktop/';
+        }
+        else {
+            alert('Since you are not running Google Chrome, some features in Cosmos of Games may or may not work.');
+        }
+    }
+    if(swfobject.hasFlashPlayerVersion("0.0.0")) {
+        if(confirm('Most games on Cosmos of Games can only be enjoyed with Adobe Flash Player, which you do not have. Do you want to go download it now?')) {
+            location = 'https://get.adobe.com/flashplayer/';
+        }
+        else {
+            alert('We recommend you go download Adobe Flash Player when possible.');
+        }
+    }
+}
+
+
+
+
+
 /*if(window.chrome === undefined) {
     viewerholder.innerHTML += '<br><div style="border: 5px solid blue; border-radius: 1vw; width: 200px; text-align: center;"><a href="https://www.google.com/chrome/browser/desktop/"><img src="http://img.talkandroid.com/uploads/2015/11/Chrome-Logo.png" style="height: 10vw; "></a><br><span style="color: white; font-size: 1.2em;">Cosmos of Games is designed for Google Chrome, the fast, simple, and secure web browser built for the modern web. If you do not have Chrome, click the Chrome icon to go download it.</span></div>';
 }*/
-if (window.chrome === undefined) {
-    if (confirm('Cosmos of Games works best on the Google Chrome browser. You do not seem to be running Google Chrome. Do you want to go download it now?')) {
-        location = 'https://www.google.com/chrome/browser/desktop/';
-    }
-    else {
-        alert('Since you are not running Google Chrome, some features in Cosmos of Games may or may not work.');
-    }
-}
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    alert('Cosmos of Games requires a mouse and keyboard. Please use your desktop computer or laptop.');
-}
