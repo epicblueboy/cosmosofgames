6ae0d5f62bca0508fab0fe231062df41e3b9cf4e
Xtraloudninja <varun.ramani@gmail.com>
2016-04-29 22:25:50 -0700

    Undid previous commit

--- otherjs.js
+++ otherjs.js
@@ -5,6 +5,6 @@
     }
 };*/
 var isChrome = !!window.chrome && !!window.chrome.webstore;
-if (!isChrome) {
-    document.getElementById('viewerholder').innerHTML = '<br><br><br><br><div style="border: 5px solid blue; border-radius: 1vw; width: 200px; text-align: center;"><a href="https://www.google.com/chrome/browser/desktop/"><img src="http://img.talkandroid.com/uploads/2015/11/Chrome-Logo.png" style="height: 10vw; "></a><br><span style="color: white; font-size: 1.2em;">Cosmos of Games is designed for Google Chrome, the fast, simple, and secure web browser built for the modern web. If you do not have Chrome, click the Chrome icon to go download it.</span></div><div style="border: 5px solid blue; border-radius: 1vw; width: 200px; text-align: center;"><br><span style="color: white; font-size: 1.2em;">Cosmos of Games works best in a maximized browser window.</span></div>'
+if (isChrome === true) {
+    document.getElementById('viewerholder').innerHTML = ''
 }
