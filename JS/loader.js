76905eaa94a698d0988c4edc9ed0a7fcbc35f985
Xtraloudninja <varun.ramani@gmail.com>
2016-05-14 16:24:32 -0700

    Updated Loader

--- JS/loader.js
+++ JS/loader.js
@@ -4,6 +4,8 @@ function adddeg() {
     deg += 1;
     loadicon.style.transform = "rotate(" + deg + "deg)";
     console.log(loadicon.style.transform);
-    window.setTimeout(adddeg, 5);
+    if (!(document.readyState === "complete")) {
+        window.setTimeout(adddeg, 1);
+    }
 }
 adddeg();
