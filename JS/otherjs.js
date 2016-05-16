ce38678e7cbd017cdc867d27f266b650ee0d45e7
Xtraloudninja <varun.ramani@gmail.com>
2016-05-16 16:56:08 -0700

    Added Open Source INfo

--- JS/otherjs.js
+++ JS/otherjs.js
@@ -15,13 +15,16 @@ $(window).load(setTimeout(function() {
     loadertext.style.zIndex = "-1";
     document.getElementById('music').play();
      document.getElementById('hometext').style.animation="hometextanim 3s";
+    document.getElementById('opensource').style.animation = "opensourceanim 2s"
     document.getElementById('buttonholder').style.animation="slidein 3s linear";    
     var backnum = Math.floor((Math.random() * 14) + 1);
     var body = document.getElementsByTagName('html')[0];
     body.style.backgroundImage = "url('./images/backgrounds/background" + backnum + ".jpg')";
 }, 4000));
 var viewerholder = document.getElementById('viewerholder');
-
+function opensource() {
+    document.getElementById('opensource').innerHTML = "<p id='opendescription'>Open Source Information:<br>Since Cosmos of Games is open source, that means you can contribute to it, and also obtain the source code. Cosmos of Games is hosted on Github at <a style='color: cyan' href='http://github.com/Xtraloudninja/cosmosofgames'>github.com/Xtraloudninja/cosmosofgames</a>. To contribute, you can fork the repository, make changes, and submit a pull request for your code to be merged into the main branch. Just think - the Greatest Gaming Website in the Universe could be running your code inside! You can also simply obtain a copy of the source code, by downloading the zip. However, that source code is for you and you only. If you intent to redistribute a project, or display it anywhere other than your own residence, you may not use the Cosmos of Games source code in it. This includes creating a spin-off of Cosmos of Games and publishing it as your own site. Just don't do it. It's simply sleazy.</p>"
+}
 if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     alert('Cosmos of Games works best with a mouse and keyboard. Please use your desktop computer or laptop.');
 }
