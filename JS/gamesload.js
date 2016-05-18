0cf75fa6b53629f88dbde8f1b0ed81afd1763d57
Xtraloudninja <varun.ramani@gmail.com>
2016-05-18 16:38:44 -0700

    Added games, updated animations

--- JS/gamesload.js
+++ JS/gamesload.js
@@ -61,6 +61,9 @@ function loadgame(game) {
         case 'agario':
             viewerholder.innerHTML = '<embed class="game" style="" src="http://agar.io" quality="high" width="80%" height="90%" scale="exactfit" />';
             break;
+        case 'diepio':
+            viewerholder.innerHTML = '<embed class="game" style="" src="https://diep.io" quality="high" width="80%" height="90%" scale="exactfit" />';
+            break;
         case 'wingsio':
             viewerholder.innerHTML = '<embed class="game" style="" src="http://wings.io" quality="high" width="80%" height="90%" scale="exactfit" />';
             break;
@@ -87,6 +90,12 @@ function loadgame(game) {
             break;
         case 'stickwar':
             viewerholder.innerHTML = '<embed class="game" style="" src="./games/stickwar.swf" quality="high" width="70%" height="80%" scale="exactfit" />';
+            break;
+        case 'onslaught':
+            viewerholder.innerHTML = '<embed class="game" style="" src="./games/onslaught.swf" quality="high" width="70%" height="80%" scale="exactfit" />';
+            break;
+        case 'onslaught2':
+            viewerholder.innerHTML = '<embed class="game" style="" src="./games/onslaught2.swf" quality="high" width="70%" height="80%" scale="exactfit" />';
             break;
         case 'tombrunner':
             viewerholder.innerHTML = '<embed class="game" style=" width: 80%; height: 80%; " src="http://files.cdn.spilcloud.com/webgl/TR-13-01/index.html?gp=1&countryCode=US&siteid=500&channelid=100&siteLocale=en-US&spilStorageId=9829247031" quality="high" scale="exactfit" />';
